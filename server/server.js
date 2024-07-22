1ca33aea5589bcef29d9fbb12dbe084f47df258e
Pat Kenny <110920919+pkay7691@users.noreply.github.com>
2024-07-22 13:42:56 -0500

    front end and backend connected

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 
 const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+
 const app = express();
-const port = 3001;
+const PORT = process.env.PORT || 5000;
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
 });
